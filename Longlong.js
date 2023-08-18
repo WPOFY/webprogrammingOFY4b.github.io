8334be3205c83317c597ab7c4b8daa68f1c17f55
WPOFY <webprogrammingofy@gmail.com>
2023-08-18 14:55:13 +0700

    first commit

---
+++ Longlong.js
@@ -0,0 +1,10 @@
+function func(){
+    var Name = document.getElementById("Name").value;
+    if (Name == "Celline") {
+        alert("Succesfull ! ")
+        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+    }
+    else {
+        alert("Unidentified ! ")
+    }
+}
